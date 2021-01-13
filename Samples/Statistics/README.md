2b3c21b284826debcc8b87934b4432e5430ac7d6
keerthana rajamanickam <keerthana.rajamanickam@gmail.com>
2021-01-13 11:03:42 +0530

    WPF-52438 Added wpf sample about get page count,get Paragraph Count, get word count and current page number properties

---
+++ Samples/Statistics/README.md
@@ -0,0 +1,2 @@
+# UWP-RichTextBox-Examples
+This repository contains examples to view ParagraphCount, PageCount, wordCount and currentPageNumber's rich text content in WPF applications using Syncfusion WPF RichTextBox control.
---
+++ Samples/Statistics/Statistics/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Statistics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            richTextBoxAdv.Load(@"..\..\Assets\Statistics.docx");
+            richTextBoxAdv.SelectionChanged += RichTextBoxAdv_SelectionChanged;
+        }
+
+        private void RichTextBoxAdv_SelectionChanged(object obj, Syncfusion.Windows.Controls.RichTextBoxAdv.SelectionChangedEventArgs args)
+        {
+            int wordCount = richTextBoxAdv.WordCount;
+            WordCount.Text = wordCount.ToString();
+            int paragraphCount = richTextBoxAdv.ParagraphCount;
+            ParagraphCount.Text = paragraphCount.ToString();
+            int pageCount = richTextBoxAdv.PageCount;
+            PageCount.Text = pageCount.ToString();
+            int currentPageNumber = richTextBoxAdv.CurrentPageNumber;
+            CurrentPageNumber.Text = currentPageNumber.ToString();
+        }
+    }
+}
